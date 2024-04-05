4d83fbc24c79a65178e8488b428ad4b1290b4c05
BBuf <1182563586@qq.com>
2024-04-05 21:40:42 +0800

    release v0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ import os
 import sys
 import sysconfig
 import shutil
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="flash_rwkv",
-    version="0.1",
-    packages=["flash_rwkv"],
+    version="0.1.3",
+    packages=find_packages(),
+    package_data={
+        'flash_rwkv': ['rwkv5/*.cu', 'rwkv5/*.cpp'],
+    },
     ext_modules=[],
     install_requires=[
         "torch>=1.13.0"
