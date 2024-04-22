119a6abc96890bc3369fcba731fa69e76ad3af8c
BBuf <1182563586@qq.com>
2024-04-22 01:34:19 +0000

    release 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flash_rwkv",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     package_data={
         'flash_rwkv': ['rwkv5/*.cu', 'rwkv5/*.cpp'],
