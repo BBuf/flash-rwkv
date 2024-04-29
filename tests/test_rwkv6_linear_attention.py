8447d65e9b625fb0329370f6cd90c242884fb395
BBuf <1182563586@qq.com>
2024-04-29 12:00:24 +0800

    fix rwkv6 test

--- tests/test_rwkv6_linear_attention.py
+++ tests/test_rwkv6_linear_attention.py
@@ -1,4 +1,5 @@
 import torch
+from fla.ops.rwkv6.recurrent_fuse import fused_recurrent_rwkv6
 
 def naive_recurrent_rwkv6(
     q,
@@ -11,24 +12,15 @@ def naive_recurrent_rwkv6(
 ):
     orig_dtype = q.dtype
     q, k, v, w, u = map(lambda x: x.float(), (q, k, v, w, u))
-
-    batch, seq_length, _ = q.shape
-    num_heads, head_size = u.shape
-    k = k.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2) # B, T, H, K -> B, H, T, K
-    v = v.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2) # B, T, H, K - > B, H, T, V
-    q = q.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2) # B, H, T, K
-    w = torch.exp(-torch.exp(w.float())).view(batch, seq_length, num_heads, head_size).permute(0, 2, 1, 3) # B, T, H, K -> B, H, T, K
-    u = u.float().reshape(num_heads, head_size) # H, K
-
-    # batch_size, n_heads, seq_len, d_head_k = q.shape
-    # _, _, _, d_head_v = v.shape
-    h = torch.zeros(batch, num_heads, head_size, head_size, dtype=torch.float32, device=q.device)
+    batch_size, n_heads, seq_len, d_head_k = q.shape
+    _, _, _, d_head_v = v.shape
+    h = torch.zeros(batch_size, n_heads, d_head_k, d_head_v, dtype=torch.float32, device=q.device)
     o = torch.zeros_like(v)
 
     if initial_state is not None:
         h += initial_state
 
-    for i in range(seq_length):
+    for i in range(seq_len):
         q_i = q[:, :, i, :]
         k_i = k[:, :, i]
         v_i = v[:, :, i, :]
@@ -39,7 +31,70 @@ def naive_recurrent_rwkv6(
         h = h * w_i[..., None] + kv_i
     return o.to(orig_dtype)
 
-def rwkv6_linear_attention_cpu(receptance, key, value, time_decay, time_first, state):
+
+def naive_recurrent_rwkv6_bwd(
+    q,
+    k,
+    v,
+    w,
+    u,
+    o,
+    do,
+    initial_state=None,
+    output_final_state=False
+):
+    q, k, v, w, u, o, do = map(lambda x: x.float(), (q, k, v, w, u, o, do))
+    batch_size, n_heads, seq_len, d_head_k = q.shape
+    _, _, _, d_head_v = v.shape
+    h = torch.zeros(batch_size, n_heads, d_head_k, d_head_v, dtype=torch.float32, device=q.device)
+    dq = torch.zeros_like(q)
+    dq_aux = torch.zeros_like(q)
+
+    if initial_state is not None:
+        h += initial_state
+
+    for i in range(seq_len):
+        k_i = k[:, :, i]
+        v_i = v[:, :, i]
+        w_i = w[:, :, i].exp()
+        kv_i = k_i[..., None] * v_i[..., None, :]
+        h_i = (h + u[None, ..., None] * kv_i)
+        dq_i = (do[:, :, i, None, :] * h_i).sum(-1)
+        dq_aux_i = (do[:, :, i, None, :] * h).sum(-1)
+        dq[:, :, i] = dq_i
+        dq_aux[:, :, i] = dq_aux_i
+        h = h * w_i[..., None] + kv_i
+
+    du = torch.zeros_like(u)
+    dh = torch.zeros_like(h)
+    dk = torch.zeros_like(k)
+    dk_aux = torch.zeros_like(k)
+    dv = torch.zeros_like(v)
+
+    for i in range(seq_len-1, -1, -1):
+        d_kv_i = do[:, :, i, None, :] * q[:, :, i, :, None]
+        k_i = k[:, :, i]
+        v_i = v[:, :, i]
+        du_i = (d_kv_i * k_i[..., None] * v_i[..., None, :]).sum(-1)
+        du += du_i
+        dk_i = (dh * v_i[..., None, :]).sum(-1)
+        dk_aux[:, :, i] = dk_i
+        dk_i += (d_kv_i * u[None, ..., None] * v_i[..., None, :]).sum(-1)
+        dv_i = (d_kv_i * u[None, ..., None] * k_i[..., None]).sum(-2)
+        dv_i += (dh * k_i[..., None]).sum(-2)
+
+        dk[:, :, i] = dk_i
+        dv[:, :, i] = dv_i
+        dh = dh * w[:, :, i, :, None].exp() + d_kv_i
+
+    # dw = q * dq_aux - k * dk_aux
+    dw = torch.zeros_like(w)
+    for i in range(seq_len-2, -1, -1):
+        dw[:, :, i] = dw[:, :, i+1] + dq_aux[:, :, i+1] * q[:, :, i+1] - dk_aux[:, :, i] * k[:, :, i]
+
+    return dq, dk, dv, dw, du
+
+def hf_rwkv6_linear_attention_cpu(receptance, key, value, time_decay, time_first, state):
     # For CPU fallback. Will be slower and probably take more memory than the custom CUDA kernel if not executed
     # within a torch.no_grad.
     batch, seq_length, _ = receptance.shape
@@ -63,6 +118,8 @@ def rwkv6_linear_attention_cpu(receptance, key, value, time_decay, time_first, s
 
     return out, state
 
+
+
 if __name__ == "__main__":
     B = 1
     H = 32
@@ -70,6 +127,7 @@ if __name__ == "__main__":
     D = 64
     HIDDEN_SIZE = H * D
     dtype = torch.float32
+    # HF Impl
     q = (torch.randn(B, L, HIDDEN_SIZE).cuda().to(torch.float16)).requires_grad_(True)
     k = (torch.randn(B, L, HIDDEN_SIZE).cuda().to(torch.float16)).requires_grad_(True)
     v = torch.randn(B, L, HIDDEN_SIZE).cuda().to(torch.float16).requires_grad_(True)
@@ -77,13 +135,19 @@ if __name__ == "__main__":
     u = (torch.randn(H, D).cuda().to(torch.float16)).requires_grad_(True)
     state = (torch.randn(B, H, D, D).cuda().to(torch.float32)).requires_grad_(True)
     
-    o1 = naive_recurrent_rwkv6(q, k, v, w, u, initial_state=state).transpose(1, 2)
-    print('o1.shape: ', o1.shape)
-    print('o1 first: ', o1.cpu().detach().numpy().flatten()[:10])
-    print('o1 end: ', o1.cpu().detach().numpy().flatten()[-10:])
-    o2, _ = rwkv6_linear_attention_cpu(q, k, v, w, u, state)
-    print('o2.shape: ', o2.shape)
-    print('o2 first: ', o2.cpu().detach().numpy().flatten()[:10])
-    print('o2 end: ', o2.cpu().detach().numpy().flatten()[-10:])
-
+    o1, _ = hf_rwkv6_linear_attention_cpu(q, k, v, w, u, state)
     
+    # Naive Recurrent RWKV6
+    batch, seq_length, _ = q.shape
+    num_heads, head_size = u.shape
+    k = k.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2) # B, T, H, K -> B, H, T, K
+    v = v.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2) # B, T, H, K - > B, H, T, V
+    q = q.float().view(batch, seq_length, num_heads, head_size).transpose(1, 2) # B, H, T, K
+    w = -torch.exp(w.float()).view(batch, seq_length, num_heads, head_size).permute(0, 2, 1, 3) # B, T, H, K -> B, H, T, K
+    u = u.float().reshape(num_heads, head_size) # H, K
+
+    o2 = naive_recurrent_rwkv6(q, k, v, w, u, initial_state=state).transpose(1, 2)
+    assert o1.allclose(o2.to(torch.float32), 0, 1e-1), breakpoint()
+    
+    o3, _ = fused_recurrent_rwkv6(q, k, v, w, u, initial_state=state, scale=1.0)
+    assert o1.allclose(o3.transpose(1, 2).to(torch.float32), 0, 1e-1), breakpoint()
